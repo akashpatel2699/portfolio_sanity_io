7da66925d9e32bc6fac871ab23ea16ef84e0106f
Akash Patel <amp228@njit.edu>
2021-01-15 10:57:32 -0500

    Basic React Route setup done with all required components setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Project';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route component={Home} path="/" exact/>
+        <Route component={About} path="/about" />
+        <Route component={Project} path="/project" />
+        <Route component={Contact} path="/contact" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1 className="cursive">Akash Patel</h1>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/project">Projects</a>
+            <a href="/contact">Contact</a>
+        </div>
+    )
+}
+
+export default NavBar
